97c4689aac232c6f53aa8ed1f351b4bb8e46db64
ThisIsMeAlex <thisisme.alessandro@gmail.com>
2022-08-28 20:46:36 +0200

    aggiunti ultimi preparitivi per la scrittura su file

--- src/main/java/business/UserService.java
+++ src/main/java/business/UserService.java
@@ -3,5 +3,6 @@ package business;
 import domain.User;
 
 public interface UserService {
+    // scrivere il metodo che legge su file gli utenti
     User authenticate(String username, String password) throws UserNotFoundException, BusinessException;
 }
--- src/main/java/controllers/LoginViewController.java
+++ src/main/java/controllers/LoginViewController.java
@@ -1,14 +1,38 @@
 package controllers;
 
+import business.BusinessException;
+import business.NftHubFactory;
+import business.UserService;
+import business.fileImplementation.FileNftHubBusinessFactory;
 import domain.Guest;
 import domain.User;
+import javafx.fxml.Initializable;
 import start.DataInitializable;
 import viewManagement.ViewDispatcher;
 
-public class LoginViewController implements DataInitializable<User>{
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 
+public class LoginViewController implements DataInitializable<User>, Initializable {
+
+    private UserService userService;
+
+    private ViewDispatcher dispatcher;
+
+    public LoginViewController() {
+        NftHubFactory factory = NftHubFactory.getInstance();
+        userService = factory.getUserService();
+
+        dispatcher = ViewDispatcher.getInstance();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
 
 
     public void goToScene2(){
@@ -17,5 +41,11 @@ public class LoginViewController implements DataInitializable<User>{
         ViewDispatcher.getInstance().loggedIn(g);
         ViewDispatcher.getInstance().renderView("2",g);
     }
+
+    public void loginAction() throws BusinessException {
+        User user = userService.authenticate("a","a");
+        dispatcher.loggedIn(user);
+    }
+
 
 }
